ec91adc81ef9a9a03924b90c70a8d9c011d45c01
CEE <archmagece@users.noreply.github.com>
2025-02-05 01:08:32 +0900

    naver icon

--- plugin.rb
+++ plugin.rb
@@ -12,11 +12,12 @@ enabled_site_setting :enable_login_with_naver
 # gem "omniauth-naver"
 # gem "sb-omniauth-naver"
 
-register_svg_icon "fab-naver"
+# register_asset "stylesheets/discourse-custom-oauth-icons.scss"
+register_svg_icon "naver"
 
 require_relative "lib/omniauth/strategies/naver"
 
 require_relative "lib/auth/login_with_naver_authenticator"
 require_relative "lib/validators/enable_login_with_naver_validator"
 
-auth_provider authenticator: Auth::LoginWithNaverAuthenticator.new, icon: "fab-naver"
+auth_provider authenticator: Auth::LoginWithNaverAuthenticator.new, icon: "naver"
