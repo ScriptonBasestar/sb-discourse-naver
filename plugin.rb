2aa257709c6627d9f63dc60e5f80165d99711ea4
CEE <archmagece@users.noreply.github.com>
2025-09-26 08:48:18 +0900

    Inline naver OAuth strategy

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # name: sb-discourse-naver
 # about: discourse plugin for Naver login and other features
 # meta_topic_id:
-# version: 0.1.0
+# version: 0.2.0
 # authors: archmagece
 # url: https://github.com/scriptonbasestar/sb-discourse-naver
 
@@ -12,17 +12,9 @@ require_relative "lib/validators/enable_login_with_naver_validator"
 
 enabled_site_setting :enable_login_with_naver
 
-gem "sb-omniauth-naver", "0.3.5"
-# gem 'sb-omniauth-naver', git: 'git@github.com:ScriptonBasestar/sb-omniauth-naver.git'
-
-# register_asset "stylesheets/discourse-custom-oauth-icons.scss"
 register_svg_icon "naver"
 
-after_initialize do
-  require "sb-omniauth-naver"
-  # require_relative "lib/omniauth/strategies/naver"
+require_relative "lib/omniauth/strategies/naver"
+require_relative "lib/auth/login_with_naver_authenticator"
 
-  require_relative "lib/auth/login_with_naver_authenticator"
-
-  auth_provider authenticator: Auth::LoginWithNaverAuthenticator.new, icon: "naver"
-end
+auth_provider authenticator: Auth::LoginWithNaverAuthenticator.new, icon: "naver"
